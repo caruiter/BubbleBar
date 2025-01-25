5aa4a30c605073aa5da435d093a8933bf23e5f1c
caruiter <99144307+caruiter@users.noreply.github.com>
2025-01-24 22:02:56 -0500

    basic drag and drop
    - player can click and hold to drag ingredients around
    - ingredient prefab added
    - players can steal ingredients from each other
    - cooldown between grabbing at 1 second

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Ingredient : MonoBehaviour
 {
+
+    private bool carrying;
+    private Player playerCarrying;
+    private Vector2 distance;
+    private float tilGrab;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+     carrying = false;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(carrying){
+            this.transform.position = new Vector2(playerCarrying.transform.position.x - distance.x,playerCarrying.transform.position.y - distance.y);
+        }
+
+        if(tilGrab>0){
+            tilGrab -= Time.deltaTime;
+        }
+    }
+
+    public void PickUp(Player p){ //player picks up the ingredient
+        if(tilGrab!<=0){
+            carrying = true;
+            playerCarrying = p;
+            distance = p.transform.position - this.transform.position;
+            p.GrabItem(this);
+        }
+    }
+
+    public void SetDown(){ //player sets down the ingredient
+        carrying = false;
+        playerCarrying.GetComponent<Player>().ReleaseItem();
+        playerCarrying = null;
+        tilGrab = 1;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,17 @@ public class Player : MonoBehaviour
     [SerializeField] private int playerID;
 
 
-	private string inputPrefix;								// InputManager uses "P1Button1", "P1Horizontal", etc. 
-    private float playerSpeed = 3;
+	private string inputPrefix;	// InputManager uses "P1Button1", "P1Horizontal", etc. 
+    private float playerSpeed = 3; //how fast does the player move?
+    public bool carrying; //is the player carrying an ingredient?
+    public Ingredient carried;
     private Rigidbody2D rb;
 
 	private void Awake()
 	{
-		inputPrefix = "P" + playerID;						// Set inputPrefix using correct playerID
+		inputPrefix = "P" + playerID; // Set inputPrefix using correct playerID
+        carrying = false;
+        carried = null;
 	}
 
     // Start is called before the first frame update
@@ -26,27 +30,55 @@ public class Player : MonoBehaviour
     void Update()
     {
 
-        Vector2 playerInput = new Vector3(
+        Vector2 playerInput = new Vector3( //find direction player is moving in
 				Input.GetAxisRaw(inputPrefix + "Horizontal"),
 				Input.GetAxisRaw(inputPrefix + "Vertical")
 				);
 
 			//transform.localPosition = Vector3.Lerp(transform.localPosition, playerInput.normalized * 0.5f, 10f * Time.deltaTime);
             //transform.localPosition = Vector3.Lerp(transform.localPosition, playerInput.normalized * 0.5f, 10f * Time.deltaTime);
-			rb.velocity = playerInput * playerSpeed;
-            //lineRenderer.SetPositions(new Vector3[] { joyTransform.position, joyContainerTransform.position });
 
-			for(int i = 0; i < 3; i++)
-			{
-				if (Input.GetButtonDown(inputPrefix + "Button" + (i + 1)))
-				{
-					
-				}
-				else if (Input.GetButtonUp(inputPrefix + "Button" + (i + 1)))
-				{
-					
-				}
-			}
+			rb.velocity = playerInput * playerSpeed; //move player
+
+
+            if (Input.GetButtonDown(inputPrefix + "Button" + (1))) //ingredient interaction button
+			    {
+                    if(!carrying) //attempt to pick up ingredient
+                        {
+                        BoxCollider2D myCollider = GetComponent<BoxCollider2D>();
+                        Collider2D[] otherColliders = Physics2D.OverlapAreaAll(myCollider.bounds.min, myCollider.bounds.max);
+
+                        // Check for any colliders that are on top
+
+                         foreach (var otherCollider in otherColliders)
+                            {
+                                Debug.Log(otherCollider.transform.name);
+                                if (otherCollider.gameObject.CompareTag("Ingredient"))
+                                    {
+                                        Debug.Log(otherCollider.transform.name + "!");
+                                        otherCollider.GetComponent<Ingredient>().PickUp(this);
+                                        break;
+                                    }
+                            }
+                        } else{ //set down what is being carried | UNCOMMENT THIS SECTION TO MAKE CARRYING EASIER
+                            /**carried.SetDown();**/
+                        }
+                } else if (Input.GetButtonUp(inputPrefix + "Button" + (1)) && carrying){ //COMMENT OUT THIS SECTION TO MAKE CARRYING HARDER
+                        //put down item when button is released
+                        carried.SetDown();
+                }
+            
+
+
         
+    }
+
+    public void GrabItem(Ingredient ingr){ //accessed by Ingredient class
+        carrying = true;
+        carried = ingr;
+    }
+    public void ReleaseItem(){ //accessed by Ingredient class
+        carrying = false;
+        carried = null;
     }
 }
